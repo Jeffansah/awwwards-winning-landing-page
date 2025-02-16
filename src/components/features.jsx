e80c18a9f3bce05599b03a0217dae24a2efc3d54
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2025-02-16 21:43:31 +0000

    Update features.jsx

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -4,7 +4,7 @@ import BentoTilt from "./utilities/bento-tilt";
 
 const Features = () => {
   return (
-    <section className="bg-black pb-52">
+    <section className="bg-black pb-2 md:pb-52 ">
       <div className="container mx-auto px-3 md:px-10">
         <div className="px-5 py-32">
           <p className="font-circular-web text-lg text-blue-50">
