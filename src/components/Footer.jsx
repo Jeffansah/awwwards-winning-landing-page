828402d768cb62df47f24d97b4059cec601c6cee
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2025-02-17 21:26:56 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const socialLinks = [
 const Footer = () => {
   return (
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
-      <div className="container mx-auto flex flex-col items-center justify-between px-4 md:flex-row">
+      <div className="container mx-auto flex flex-col gap-4 items-center justify-between px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
           ©Anavrin {new Date().getFullYear()}. All rights reserved
         </p>
